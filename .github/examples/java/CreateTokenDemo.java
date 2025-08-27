ae94988344319d960d882512d7dc3290d51b03e7
SimiHunjan <simi.hunjan.k@gmail.com>
2025-08-27 14:57:01 -0700

    Update CreateTokenDemo.java
    Signed-off-by: SimiHunjan <simi.hunjan.k@gmail.com>

--- .github/examples/java/CreateTokenDemo.java
+++ .github/examples/java/CreateTokenDemo.java
@@ -56,7 +56,7 @@ public class CreateTokenDemo {
 
         // Wait for Mirror Node to populate data
         System.out.println("\nWaiting for Mirror Node to update...");
-        Thread.sleep(10000);
+        Thread.sleep(100000);
 
         // query balance using Mirror Node
         String mirrorNodeUrl = mirrorNode + 
